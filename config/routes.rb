1e497096ee88c04cdbc8183fcb48d444135c86e5
malanchristiansen <malanschristiansen@gmail.com>
2021-08-04 19:50:04 +1000

    added payment method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'payment/page', to: "payment#create", as: 'payment'
+  get 'payment/cancel', to: "payment#cancel", as: 'payment_cancel'
   resources :reviews
   resources :listings
   resources :profiles
