32d319e3eac5fbed894defcad7bd0b2dc3828f11
malanchristiansen <malanschristiansen@gmail.com>
2021-08-04 14:03:32 +1000

    created user journey for buyer and returning user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+    before_action :set_user_type
+
+    def set_user_type
+        @user_type = params[:user_type]
+    end 
+
     def after_sign_in_path_for(profile)
         if(current_user.profile)
-            new_listing_path
+            if(params[:user][:user_type] == "buyer" || "returning_user")
+                root_path
+            else 
+                new_listing_path
+            end
         else 
-            new_profile_path || root_path
+            new_profile_path(user_type: params[:user][:user_type]) || root_path
         end 
     end
 end
