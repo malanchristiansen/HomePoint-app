75985f564e91efdd6b6f9df9d39c3b4c93334e49
malanchristiansen <malanschristiansen@gmail.com>
2021-08-08 12:06:44 +1000

    added validations to listing and profile model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,12 @@ class Listing < ApplicationRecord
   #belongs_to :profile
   belongs_to :buyer, class_name: "Profile", optional: true 
   belongs_to :seller, class_name: "Profile"
-
   has_one :location
-
   has_many_attached :images 
+
+  # Validations 
+  validates :title, :category, :price, presence: true
+  validates :description, length: { maximum: 400, too_long: "has a maximun of %{count} charcaters!" }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,8 +2,12 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :photographs_to_buy, class_name: "Listing", foreign_key: "buyer_id"
   has_many :photographs_to_sell, class_name: "Listing", foreign_key: "seller_id"
-
   has_many :reviews 
-
   has_one_attached :avatar
+
+  # Validations 
+  validates :username, :first_name, presence: true
+  validates :username, length: { in: 2..16 }
+  validates :bio, length: { maximum: 500, too_long: "%{count} is the maximum number of charcaters!" }
+
 end
