5d945cdf509c70125bc612cf37d2f30391064c07
malanchristiansen <malanschristiansen@gmail.com>
2021-08-10 14:28:23 +1000

    integrating stripe on heroku

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,5 @@
+Rails.configuration.stripe = {
+    :public_key => ENV['STRIPE_PUBLIC_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+}
+Stripe.api_key = Rails.configuration.stripe[:stripe_api_key]
